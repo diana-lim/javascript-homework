35fa6ddf576ba8a72fea7b8d9267c8bd7537d792
diana-lim <lim.diana08@gmail.com>
2020-01-22 03:56:41 +0000

    Initial commit

---
+++ script.js
@@ -0,0 +1,25 @@
+function responseOne() { 
+  var a = document.getElementById("food1").value;
+  var b = document.getElementById("food2").value;
+  var c = document.getElementById("food3").value;
+  var d = document.getElementById("food4").value;
+  var e = document.getElementById("food5").value;
+
+  alert("Wow, I like to eat " + a + ", " + b + ", " + c + ", " + d + ", " + "and " + e + " too!");
+};
+
+function responseTwo() {
+  var a = document.getElementById("food1").value;
+  var b = document.getElementById("food2").value;
+  var c = document.getElementById("food3").value;
+
+   alert("Wow, I like to eat " + a + ", " + b + ", " + "and " + c + " too!");
+};
+
+function responseThree() {
+  var c = document.getElementById("food3").value;
+  var d = document.getElementById("food4").value;
+  var e = document.getElementById("food5").value;
+
+  alert("Wow, I like to eat " + c + ", " + d + ", " + "and " + e + " too!");
+};
